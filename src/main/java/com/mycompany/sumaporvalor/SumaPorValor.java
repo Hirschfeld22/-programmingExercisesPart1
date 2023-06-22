949d2945b4c656fe93eb332cc18454cf3e00512a
fscco321@gmail.com <fscco321@gmail.com>
2023-06-21 22:11:40 -0300

    initial commit

---
+++ src/main/java/com/mycompany/sumaporvalor/SumaPorValor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sumaporvalor;
+
+/**
+ *
+ * @author Francisco
+ */
+public class SumaPorValor {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado = Suma(14,23);
+        System.out.println(resultado);
+    }
+    
+    public static int Suma(int a, int b){
+        return a + b;
+    }
+}
